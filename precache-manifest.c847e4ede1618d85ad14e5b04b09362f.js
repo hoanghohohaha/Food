717c1f2a35168bdc19272054024fddaecf151e80
hoanghohohaha <hoanghohohaha@gmail.com>
2020-06-05 18:41:30 +0700

    Updates

---
+++ precache-manifest.c847e4ede1618d85ad14e5b04b09362f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497f41992a8e5f6a10377f99a73e2838",
+    "url": "/Food/index.html"
+  },
+  {
+    "revision": "5aa96103b68ef7841ce9",
+    "url": "/Food/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3ab847a999847b2e992a",
+    "url": "/Food/static/js/2.b47113b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Food/static/js/2.b47113b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa96103b68ef7841ce9",
+    "url": "/Food/static/js/main.47a553f4.chunk.js"
+  },
+  {
+    "revision": "b2313980cc3ef6ad905f",
+    "url": "/Food/static/js/runtime-main.b79c1802.js"
+  }
+]);
